c7691e1d4003cfdf0457ccbf4e138d215a68ebe0
Nathaniel Ketema <nathanielketema@gmail.com>
2026-05-16 08:21:21 -0500

    apply the parse don't validate rule

--- src/shellu.zig
+++ src/shellu.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
-const readline = @import("readline");
 const Io = std.Io;
 const mem = std.mem;
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
+
+const readline = @import("readline");
+
+pub fn maybe(ok: bool) void {
+    assert(ok or !ok);
+}
 
 pub const ShellErrors = error{
     EnvironmentVariableNotFound,
@@ -20,9 +28,51 @@ pub const BuiltinCommand = enum {
     }
 };
 
+const Input = struct {
+    command: Command,
+    args: []const []const u8,
+    gpa: Allocator,
+
+    const Command = struct {
+        builtin: ?BuiltinCommand,
+        string: []const u8,
+    };
+
+    pub fn parse(gpa: Allocator, raw_input: []u8) !Input {
+        assert(raw_input.len > 0);
+
+        var args: ArrayList([]const u8) = .empty;
+        errdefer args.deinit(gpa);
+
+        var it = mem.tokenizeAny(u8, raw_input, " ");
+        const command_string = it.next().?;
+        const command: Command = .{
+            .builtin = BuiltinCommand.parse(command_string),
+            .string = command_string,
+        };
+
+        while (it.next()) |arg| {
+            try args.append(gpa, arg);
+        }
+        maybe(args.items.len == 0);
+
+        return .{
+            .command = command,
+            .args = try args.toOwnedSlice(gpa),
+            .gpa = gpa,
+        };
+    }
+
+    pub fn deinit(input: *Input) void {
+        input.gpa.free(input.args);
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
+    const gpa = init.gpa;
     const arena = init.arena.allocator();
+
     var stdout_buffer: [4096]u8 = undefined;
     var stdout_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout = &stdout_writer.interface;
@@ -37,21 +87,24 @@ pub fn main(init: std.process.Init) !void {
     _ = readline.rl_bind_key('\t', readline.rl_complete);
     readline.using_history();
     while (true) {
-        // max is set at 40KB
+        // max is set at around 40KB
         _ = init.arena.reset(.{ .retain_with_limit = 10 * 4096 });
-        const c_cmdline: [*c]u8 = readline.readline("$ ") orelse continue;
-        defer std.c.free(c_cmdline);
-        _ = readline.add_history(c_cmdline);
 
-        const cmdline: []u8 = mem.span(c_cmdline);
-        var cmdline_it = mem.tokenizeAny(u8, cmdline, " ");
-        const command_string = cmdline_it.next() orelse continue;
+        const raw_input_c: [*c]u8 = readline.readline("$ ") orelse continue;
+        defer std.c.free(raw_input_c);
+        const raw_input: []u8 = mem.span(raw_input_c);
 
-        if (BuiltinCommand.parse(command_string)) |command| {
-            switch (command) {
+        if (raw_input.len == 0) continue;
+        _ = readline.add_history(raw_input_c);
+
+        var input: Input = try .parse(gpa, raw_input);
+        defer input.deinit();
+
+        if (input.command.builtin) |builtin| {
+            switch (builtin) {
                 .echo => {
                     var first: bool = true;
-                    while (cmdline_it.next()) |arg| {
+                    for (input.args) |arg| {
                         if (!first) try stdout.print(" ", .{});
                         try stdout.print("{s}", .{arg});
                         first = false;
@@ -59,23 +112,26 @@ pub fn main(init: std.process.Init) !void {
                     try stdout.print("\n", .{});
                 },
                 .type => {
-                    const arg_string = cmdline_it.next() orelse continue;
-                    if (BuiltinCommand.parse(arg_string)) |arg| {
-                        try stdout.print("{s} is a shell builtin\n", .{@tagName(arg)});
-                    } else {
+                    if (input.args.len == 0) continue;
+                    for (input.args) |arg| {
+                        if (BuiltinCommand.parse(arg)) |cmd| {
+                            try stdout.print("{s} is a shell builtin\n", .{@tagName(cmd)});
+                            continue;
+                        }
+
                         var path_it = std.mem.tokenizeAny(u8, path, " :;");
                         while (path_it.next()) |sub_path| {
                             const dir = Io.Dir.openDir(.cwd(), io, sub_path, .{}) catch continue;
 
-                            if (dir.access(io, arg_string, .{ .execute = true })) |_| {
+                            if (dir.access(io, arg, .{ .execute = true })) |_| {
                                 try stdout.print("{s} is {s}/{s}\n", .{
-                                    arg_string,
+                                    arg,
                                     sub_path,
-                                    arg_string,
+                                    arg,
                                 });
                                 break;
                             } else |_| continue;
-                        } else try stdout.print("{s}: not found\n", .{arg_string});
+                        } else try stdout.print("{s}: not found\n", .{arg});
                     }
                 },
                 .pwd => {
@@ -83,11 +139,18 @@ pub fn main(init: std.process.Init) !void {
                     try stdout.print("{s}\n", .{current_path});
                 },
                 .cd => {
-                    const arg_string = cmdline_it.next() orelse continue;
-                    const dir_path = if (mem.startsWith(u8, arg_string, "~"))
-                        try mem.concat(arena, u8, &.{ home, arg_string[1..] })
+                    if (input.args.len > 1) {
+                        try stdout.print("Error: too many arguments provided\n", .{});
+                        try stdout.print("Usage: cd [path]\n", .{});
+                        try stdout.flush();
+                        continue;
+                    }
+
+                    const arg = if (input.args.len == 0) home else input.args[0];
+                    const dir_path = if (mem.startsWith(u8, arg, "~"))
+                        try mem.concat(gpa, u8, &.{ home, arg[1..] })
                     else
-                        arg_string;
+                        arg;
 
                     Io.Threaded.chdir(dir_path) catch {
                         try stdout.print("cd: {s}: No such file or directory\n", .{dir_path});
@@ -97,21 +160,29 @@ pub fn main(init: std.process.Init) !void {
                     const history_state: [*c]readline.HISTORY_STATE = readline.history_get_history_state();
                     const history_count: usize = @intCast(history_state.*.length);
 
-                    if (cmdline_it.next()) |arg| {
+                    if (input.args.len > 1) {
+                        try stdout.print("Error: too many arguments provided\n", .{});
+                        try stdout.print("Usage: history [limit]\n", .{});
+                        try stdout.flush();
+                        continue;
+                    }
+
+                    var start: usize = 1;
+                    if (input.args.len == 1) {
+                        const arg = input.args[0];
                         const limit = try std.fmt.parseInt(u8, arg, 10);
-                        if (limit > history_count) @panic("don't do that");
-                        const start = history_count - limit + 1;
-                        for (start..history_count + 1) |i| {
-                            const entry: [*c]readline.HIST_ENTRY = readline.history_get(@intCast(i)) orelse continue;
-                            const line: [*c]const u8 = entry.*.line orelse continue;
-                            try stdout.print("    {d} {s}\n", .{ i, mem.span(line) });
+                        if (limit > history_count) {
+                            try stdout.print("Error: limit exceeds history count\n", .{});
+                            try stdout.flush();
+                            continue;
                         }
-                    } else {
-                        for (1..history_count + 1) |i| {
-                            const entry: [*c]readline.HIST_ENTRY = readline.history_get(@intCast(i)) orelse continue;
-                            const line: [*c]const u8 = entry.*.line orelse continue;
-                            try stdout.print("    {d} {s}\n", .{ i, mem.span(line) });
-                        }
+                        start = history_count - limit + 1;
+                    }
+
+                    for (start..history_count + 1) |i| {
+                        const entry: [*c]readline.HIST_ENTRY = readline.history_get(@intCast(i)) orelse continue;
+                        const line: [*c]const u8 = entry.*.line orelse continue;
+                        try stdout.print("    {d} {s}\n", .{ i, mem.span(line) });
                     }
                 },
                 .exit => return,
@@ -121,23 +192,30 @@ pub fn main(init: std.process.Init) !void {
             while (path_it.next()) |sub_path| {
                 const dir = Io.Dir.openDir(.cwd(), io, sub_path, .{}) catch continue;
 
-                if (dir.access(io, command_string, .{ .execute = true })) |_| {
-                    var argv: std.ArrayList([]const u8) = .empty;
-                    cmdline_it.reset();
-                    while (cmdline_it.next()) |arg| {
-                        if (mem.startsWith(u8, arg, "~")) {
-                            try argv.append(
-                                arena,
-                                try mem.concat(arena, u8, &.{ home, arg[1..] }),
-                            );
-                        } else try argv.append(arena, arg);
-                    }
+                var argv: ArrayList([]const u8) = .empty;
+                defer argv.deinit(gpa);
 
+                try argv.append(gpa, input.command.string);
+                for (input.args) |arg| {
+                    const ready_arg = if (mem.startsWith(u8, arg, "~"))
+                        try mem.concat(
+                            gpa,
+                            u8,
+                            &.{ home, arg[1..] },
+                        )
+                    else
+                        arg;
+
+                    try argv.append(gpa, ready_arg);
+                }
+
+                assert(argv.items.len > 0);
+                if (dir.access(io, input.command.string, .{ .execute = true })) |_| {
                     var child = try std.process.spawn(io, .{ .argv = argv.items });
                     _ = try child.wait(io);
                     break;
                 } else |_| continue;
-            } else try stdout.print("{s}: command not found\n", .{command_string});
+            } else try stdout.print("{s}: command not found\n", .{input.command.string});
         }
 
         try stdout.flush();
